1f9a604bb7a3be89680fa61ae5b9cc5c96cef830
Tinco Andringa <mail@tinco.nl>
2012-06-23 13:59:03 +0200

    new rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DoneCrypto::Application.config.secret_token = 'dcb99cdfeac84e36a80f31ef56e0d0bb71932c3abc29d3fbf62d9f585460b7b4b47ee2b5d290b108f2f20563a67c2829d7dec9e82e4aeb4eadd73c4dda04b6b3'
